61964db48c943ab462453e0a2981b20602d5f198
lennne <kobiansah@gmail.com>
2025-12-15 08:35:26 -0330

    update: cors configuration for express

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const app = express()
-const PORT = process.env.PORT;
+const cors = require('cors')
+
+const corsOptions = require('./config/corsOptions')
 const { logger } = require('./middleware/logger')
 const { errorHandler } = require('./middleware/errorHandler')
 
-
+const app = express()
+const PORT = process.env.PORT;
 
 app.use(logger)
 
+app.use(cors(corsOptions)) 
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
