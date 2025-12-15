5a43ec4d7c69644adb09d53ad277918dfc6c2ad2
lennne <kobiansah@gmail.com>
2025-12-15 11:32:58 -0330

    update: implement user routes and create/read controller logic
    Define /users routing for GET, POST, PATCH, DELETE
    
    Implement getAllUsers with password exclusion
    
    Implement createNewUser with validation and bcrypt hashing
    
    Add placeholders for update and delete operations

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require('express')
 
 const router = express.Router()
 
+const usersController  = require('../controllers/usersController')
+
 //all '/users' arrive in this router, meaning '/users/'
 router.route('/')
-    .get()
-    .post()
-    .patch()
-    .delete()
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUsers)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser)
 
 module.exports = router
