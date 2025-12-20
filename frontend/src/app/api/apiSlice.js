cc2bcfff3aa26484dfbf8ab2147e04ff663917ae
lennne <kobiansah@gmail.com>
2025-12-20 18:19:31 -0330

    fix: resolve dependency location and setup redux provider
    Move @reduxjs/toolkit and react-redux from root to frontend/package.json
    
     Remove react-router from root to prevent duplicate React instances
    
    Wrap App component with Redux Provider in index.js

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,4 +1,5 @@
-import { createApi, fetchBaseQuery } from '@redujs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 
 //essentially what we would use axios for
 export const apiSlice = createApi({
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
